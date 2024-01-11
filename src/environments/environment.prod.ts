8c46b2e39210691ce62d67f66ea6e2f0dcc5a642
Rodrigo Almeida <rodrigoalmeida08@hotmail.com>
2024-01-11 12:25:27 +0000

    built for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAFj8VX3MXzqCuszc815NtvGzOOpy6ISu8',
+    authDomain: 'finance-manager-a20d9.firebaseapp.com',
+    projectId: 'finance-manager-a20d9',
+    storageBucket: 'finance-manager-a20d9.appspot.com',
+    messagingSenderId: '144300752414',
+    appId: '1:144300752414:web:966fb02cd729db5ecef80f',
+    measurementId: 'G-0L3N9PB9Z2',
+  }
 };
