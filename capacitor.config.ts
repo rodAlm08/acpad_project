5edaf62873c2589e2face3b65520aa21a441464e
Rodrigo Almeida <rodrigoalmeida08@hotmail.com>
2024-01-11 15:53:22 +0000

    gradle changed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.rodalm.budgetplanner',
   appName: 'Budget Planner',
   webDir: 'www',
   server: {
